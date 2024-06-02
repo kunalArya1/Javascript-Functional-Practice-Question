6b557ac6a7457d5b32cd051b838d4cbb85bf9641
kunalArya1 <kunalkrraj220@gmail.com>
2024-06-02 09:44:18 +0530

    Functional Practice Question Repo

---
+++ PracticeSet1.js
@@ -0,0 +1,11 @@
+/**  1. Write an ES6 function that takes an array of numbers and returns an array with each number
+ incremented by 3 using the map method.
+
+
+ const numbers = [1, 2, 3, 4, 5]
+ console.log(incrementNumbers(numbers))
+ // Output: [4, 5, 6, 7, 8] 
+ 
+ */
+
+ 
